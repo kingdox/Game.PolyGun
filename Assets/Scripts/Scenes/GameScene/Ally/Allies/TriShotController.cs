efac0ef4d454789f19f731afd761352a6be38fd2
kingdox <arpaxavier@gmail.com>
2021-01-30 03:33:46 +0100

    no message

--- Assets/Scripts/Scenes/GameScene/Ally/Allies/TriShotController.cs
+++ Assets/Scripts/Scenes/GameScene/Ally/Allies/TriShotController.cs
@@ -15,6 +15,12 @@ public class TriShotController : Minion
 
     [Space]
     private Transform _player;
+    [Space]
+    private float refreshTargetCount;
+    [Space]
+    public float maxRangeSize = 2;
+    public float minRangeSize = 4;
+
     #endregion
     #region Events
     private void Start()
@@ -30,10 +36,46 @@ public class TriShotController : Minion
         if (UpdateMinion())
         {
 
+            //if exist a target then...
+            if (target != null)
+            {
+
+                // Target refresher
+                if (Timer(ref refreshTargetCount, 1))
+                {
+                    UpdateTarget();
+                }
+
+                //Rotation Refresh
+                if (target != transform)
+                {
+                    // rotates to that target
+                    rotation.LookTo(target.position);
+                }
 
 
+                //Player following
+                UpdatePlayerFollow();
 
+            }
+            else
+            {
+                UpdateTarget();
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        if (_player != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(_player.position, character.range);
 
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(_player.position, character.range / maxRangeSize);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(_player.position, character.range / minRangeSize);
         }
     }
     #endregion
@@ -45,7 +87,47 @@ public class TriShotController : Minion
 
     }
 
+    private void UpdatePlayerFollow()
+    {
 
+        float distance = Vector3.Distance(transform.position, _player.position);
+        int orientation = 1;
+        bool canMove = false;
+        bool isGoingToSafeArea = false;
+        bool inRange = distance > character.range / maxRangeSize && (distance < character.range / minRangeSize);
+        /*
+         * TRI SHOT
+         * 
+         *  estara cerca del player hasta cierto punto,
+         * 
+         */
+
+
+        //if (distance > character.range / maxRangeSize)
+        //{
+        //    canMove = true;
+        //}
+        //else
+        //{
+        //    if (distance < character.range / minRangeSize)
+        //    {
+        //        canMove = true;
+        //        isGoingToSafeArea = true;
+        //    }
+        //    else
+        //    {
+        //        canMove = true;
+        //    }
+
+        //}
+
+        //if (canMove)
+        //{
+
+        //}
+
+        //movement.Move(orientation * transform.forward.normalized, character.speed);
+    }
 
     #endregion
 }
